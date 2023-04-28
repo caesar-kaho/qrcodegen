48f071622dcdfe57e5b3a17323b44cfc69f7c890
caesarkaho <112843418+77LYON@users.noreply.github.com>
2023-04-28 21:09:17 +0800

    bug fixed

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
   res.render('index');
