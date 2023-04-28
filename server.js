d30bcbb4ce80e688ea4b476108f1447cecae20d7
caesarkaho <112843418+77LYON@users.noreply.github.com>
2023-04-28 20:42:28 +0800

    port changed

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running');
-});
+app.listen(process.env.PORT, () => {
+    console.log('Server is running');
+  });
+  
